9d9d749bb250c87132532c2bda08201289b69a85
CARLOS EDUARDO SANTOS VITOR <20241134040016@ifrn.local>
2025-06-25 11:27:38 -0300

    Updates

---
+++ assets/plus-VZCkCVvs.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-Atvpjqec.js";const e=s("Minus",[["path",{d:"M5 12h14",key:"1ays0h"}]]),t=s("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);export{e as M,t as P};
